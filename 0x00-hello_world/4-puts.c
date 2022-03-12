b133728c9c66a14fafbc6fdad9ff27e3cd2f4e4c
JohnnyOhms <Officialjohn662@gmail.com>
2022-03-11 16:42:19 -0800

    C programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,11 @@
-#!/bin/bash
-put("Programming is like building a multilingual puzzle \n");
+#include <studio.h>
+/**
+ * main - entry point
+ *
+ * Return : 0 after printing the function
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
