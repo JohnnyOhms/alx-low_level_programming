b3f9b16ce61eedabb7bee2e6b767690e5a1bbcc0
JohnnyOhms <Officialjohn662@gmail.com>
2022-03-14 11:48:31 -0700

    C programming

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
-*main - Entry point
-*My hello, printf task
-*Return : return 1 after programe execution
-*/
+ * main - Entry point
+ * My hello, printf task
+ * Return : return 1 after programe execution
+ */
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,\n");
+	printf("\"with proper grammar, but the outcome is a piece of art");
 	return (0);
 }
