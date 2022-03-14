381ce83be99ea6e74177ae64504db1c1af59e79a
JohnnyOhms <Officialjohn662@gmail.com>
2022-03-14 11:15:32 -0700

    C programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-/**
+/*
 * main - prints the phrase programing is like a multilingual puzzle
 * Return: 0 if existed properly, non-zero otherwise
  */
 int main(void)
 {
-      puts("\"Programming is like building a multilingual puzzle");
-               return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
