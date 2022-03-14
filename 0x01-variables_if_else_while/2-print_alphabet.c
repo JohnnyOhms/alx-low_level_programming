1408949d36ec0a50523eb232a9dc73495eec42d5
JohnnyOhms <Officialjohn662@gmail.com>
2022-03-14 16:22:07 -0700

    updated

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/*
+ * main - main Entry
+ * Description: The use of putchar fucion to print alphabet
+ * in lower case, can be used only twice.
+ * Return: 0
+ */
+int main(void)
+{
+	char c = 'a':
+	
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+
+	return (0);
+}
