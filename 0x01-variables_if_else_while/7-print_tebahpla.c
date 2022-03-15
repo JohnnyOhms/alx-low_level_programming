a07694cd0478b6db67193ec92760f3c63e2a2fc4
JohnnyOhms <Officialjohn662@gmail.com>
2022-03-15 09:43:27 -0700

    updated

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - main entry
+ * Description: print lower case alpha and new line
+ * Return: 0
+ */
+int main(void)
+{
+	char i;
+
+	for (i = z; i < a; i++)
+	{
+		putchar(i + 'z');
+	}
+	putchar('\n');
+
+	return (0);
+}
