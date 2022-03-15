77b4f6bf559f41388cd5ef5375f50acb5e2dc79a
JohnnyOhms <Officialjohn662@gmail.com>
2022-03-15 09:21:50 -0700

    updated

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Print all digit of base 10 from 0
+ * Return: 0
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 0; c < 10; c++)
+	{
+		printf("%c", c);
+	}
+	putchar('\n');
+
+	return (0);
+}
+
