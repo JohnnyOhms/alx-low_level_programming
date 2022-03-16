caa8e65d97bf8aa7c4b7b64b452f9fd99ba47798
JohnnyOhms <Officialjohn662@gmail.com>
2022-03-15 17:15:51 -0700

    updated

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -25,11 +25,7 @@ int main(void)
 	else
 	{
 		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, m);
+	}
 
 		return (0); 
 }
-
-
-
-
-
--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /*
  * main - main Entry
- * Description: The use of 'putchar' to print alpha 
+ * Description: The use of putchar to print alpha 
  * Return: 0
  */
 int main(void)
 {
 	char c = 'a';
-	
+
 	while (c <= 'z')
 	{
 		putchar(c);
