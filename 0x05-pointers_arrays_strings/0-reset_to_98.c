066cba7e94b1a0a765c874a12ee19c455e88523a
JohnnyOhms <Officialjohn662@gmail.com>
2022-03-21 17:40:11 -0700

    updated

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - update the value it points
+ * @n: pointer of thr direction at the variable n
+ * Return: 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,6 @@
+#ifndef main_h
+#define main_h
+int _Putchar(char c);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
