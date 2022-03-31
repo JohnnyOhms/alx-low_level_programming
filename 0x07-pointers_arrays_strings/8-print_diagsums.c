e3b7d57cdec24a517e30e1636d3447d82fcb5eec
JohnnyOhms <Officialjohn662@gmail.com>
2022-03-31 13:30:51 -0700

    made some corrections

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -16,4 +16,7 @@ for (i = 0; i < (size * size); i++)
 if (i % (size + 1) == 0)
 sum1 += *(a + i);
 if (i % (size - 1) == 0 && i != 0 && i < size * size - 1)
-
+sum2 += *(a + i);
+}
+printf("%d, %d\n", sum1, sum2);
+}
